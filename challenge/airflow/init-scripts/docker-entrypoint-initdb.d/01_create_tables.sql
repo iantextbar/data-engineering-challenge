14c5c65e77386dca903d9e0d075f34e41d1a9e3d
iantextbar <ian.barreiro@gmail.com>
2026-07-15 14:09:39 -0300

    beginning extraction dag

---
+++ challenge/airflow/init-scripts/docker-entrypoint-initdb.d/01_create_tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS raw_nyc_tlc_data (
+    id SERIAL PRIMARY KEY,
+    VendorID INT,
+    tpep_pickup_datetime TIMESTAMP,
+    tpep_dropoff_datetime TIMESTAMP,
+    passenger_count INT,
+    trip_distance NUMERIC,
+    RatecodeID INT,
+    store_and_fwd_flag VARCHAR(50),
+    PULocationID INT,
+    DOLocationID INT,
+    payment_type INT,
+    fare_amount NUMERIC(10, 2),
+    extra NUMERIC,
+    mta_tax NUMERIC,
+    tip_amount NUMERIC(10, 2),
+    tolls_amount NUMERIC(10, 2),
+    improvement_surcharge NUMERIC(10, 2),
+    total_amount NUMERIC(10, 2),
+    congestion_surcharge NUMERIC(10, 2),
+    airport_fee NUMERIC(10, 2)
+)
+
+CREATE TABLE IF NOT EXISTS trusted_nyc_tlc_data (
+    id SERIAL PRIMARY KEY,
+    tpep_pickup_datetime TIMESTAMP,
+    tpep_dropoff_datetime TIMESTAMP,
+    trip_distance NUMERIC
+)
